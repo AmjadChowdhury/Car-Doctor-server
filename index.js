bdcbdfba7a7445f0b5834f110bc9ab5e897e027f
AmjadChowdhury <amjadchowdhury247@gmail.com>
2025-01-27 23:16:39 +0600

    set origin in cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000
 
 //middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://car-doctors-d7fe2.web.app'],
   credentials: true
 }))
 app.use(express.json())
